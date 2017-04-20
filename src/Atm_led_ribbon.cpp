29dfb04bf3bd2a4e64b7c88d5bd695b1922a9cc7
Pavel Veretennikov <vermut@kid.lv>
2017-04-21 01:07:48 +0300

    formatting

--- src/Atm_led_ribbon.cpp
+++ src/Atm_led_ribbon.cpp
@@ -5,10 +5,10 @@ Atm_led_ribbon &Atm_led_ribbon::begin() {
     // clang-format off
     const static state_t state_table[] PROGMEM = {
             /*                           ON_ENTER  ON_LOOP  ON_EXIT  EVT_COUNTDOWN_ZERO  EVT_TIMER_TICK  EVT_MAKE_PROGRESS  EVT_WRONG_MOVE         ELSE */
-            /*          IDLE */          ENT_IDLE, -1, -1, -1, -1, INIT_PROGRESS, -1, -1,
-            /* INIT_PROGRESS */ ENT_INIT_PROGRESS, -1, -1, -1, -1, -1, -1, PROGRESSING,
-            /*   PROGRESSING */   ENT_PROGRESSING, -1, -1, FINISHED, PROGRESSING, -1, IDLE, -1,
-            /*      FINISHED */      ENT_FINISHED, -1, -1, -1, -1, -1, -1, -1,
+            /*          IDLE */          ENT_IDLE,      -1,      -1,                 -1,             -1,     INIT_PROGRESS,             -1,          -1,
+            /* INIT_PROGRESS */ ENT_INIT_PROGRESS,      -1,      -1,                 -1,             -1,                -1,             -1, PROGRESSING,
+            /*   PROGRESSING */   ENT_PROGRESSING,      -1,      -1,           FINISHED,    PROGRESSING,                -1,           IDLE,          -1,
+            /*      FINISHED */      ENT_FINISHED,      -1,      -1,                 -1,             -1,                -1,             -1,          -1,
     };
     // clang-format on
     Machine::begin(state_table, ELSE);

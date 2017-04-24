2d8fc493d147b611935f1c9e8d673d447a65cfbb
Pavel Veretennikov <vermut@kid.lv>
2017-04-24 18:38:11 +0300

    ocassionaly working modbus config

--- src/Boxes.cpp
+++ src/Boxes.cpp
@@ -5,27 +5,25 @@ extern void modbus_setup();
 extern void modbus_loop();
 extern unsigned int holdingRegs[]; // function 3 and 16 register array
 
-
 void setup() {
-	multipartLedRibbon.setup();
-	Serial.begin(57600);
-
-	button1.begin(2);
-	button2.begin(3);
-	button3.begin(4);
-	button4.begin(5);
-	button5.begin(6);
-	button6.begin(7);
-
-	led1.begin(30);
-	led2.begin(31);
-	led3.begin(32);
-	led4.begin(33);
-	led5.begin(34);
-	led6.begin(35);
-
+	Serial.begin(115200);
 	modbus_setup();
-	main_sequence.begin();
+//	multipartLedRibbon.setup();
+//	button1.begin(2);
+//	button2.begin(3);
+//	button3.begin(4);
+//	button4.begin(5);
+//	button5.begin(6);
+//	button6.begin(7);
+//
+//	led1.begin(30);
+//	led2.begin(31);
+//	led3.begin(32);
+//	led4.begin(33);
+//	led5.begin(34);
+//	led6.begin(35);
+//
+//	main_sequence.begin();
 }
 
 void loop() {

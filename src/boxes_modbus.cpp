7c8b9b466db615be41741cd7864bb2c9d12db463
Pavel Veretennikov <vermut@kid.lv>
2017-06-28 00:05:04 +0300

    working towards ModbusSerial

--- src/boxes_modbus.cpp
+++ src/boxes_modbus.cpp
@@ -1,76 +1,127 @@
-#include <SimpleModbusSlave.h>
+#define USE_HOLDING_REGISTERS_ONLY
+
+#include <Arduino.h>
+#include <Modbus.h>
+
+extern void modbus_setup();
+extern void modbus_loop();
+extern void modbus_set(word event, word value);
 
 //////////////// registers of BOXES ///////////////////
 enum {
 	// The first register starts at address 0
-			ACTIONS,      // Always present, used for incoming actions
+	ACTIONS,      // Always present, used for incoming actions
 
 	// Any registered events, denoted by 'triggered_by_register' in rs485_node of Lua script, 1 and up
-			MINOR_FAILURE,
+	MINOR_FAILURE,
 	MAJOR_FAILURE,
 	COMPLETE,
 
-	TOTAL_ERRORS,     // leave this one, error counter
-	TOTAL_REGS_SIZE   // INTERNAL: total number of registers for function 3 and 16 share the same register array
+	TOTAL_ERRORS     // leave this one, error counter
 };
 
-unsigned int holdingRegs[TOTAL_REGS_SIZE]; // function 3 and 16 register array
-////////////////////////////////////////////////////////////
+#ifdef USE_SOFTWARE_SERIAL
+#include <ModbusSerial.h>
+ModbusSerial mb;
+
+#define SSerialTxControl 6   //RS485 Direction control
+#define SSerialRX        8  //Serial Receive pin
+#define SSerialTX        9  //Serial Transmit pin
+SoftwareSerial RS485Serial(SSerialRX, SSerialTX); // RX, TX
+#endif
+
+#ifdef USE_ALT_SOFT_SERIAL
+#include <ModbusSerial.h>
+ModbusSerial mb;
+
+#define SSerialTxControl 6   //RS485 Direction control
+#define SSerialRX        8  //Serial Receive pin
+#define SSerialTX        9  //Serial Transmit pin
+AltSoftSerial RS485Serial(SSerialRX, SSerialTX); // RX, TX
+#endif
+
+#ifdef USE_SERIAL1
+#include <ModbusSerial.h>
+ModbusSerial mb;
+
+#define SSerialRX        19  //Serial3 Receive pin (just a reference, can't be changed)
+#define SSerialTX        18  //Serial3 Transmit pin (just a reference, can't be changed)
+#define SSerialTxControl 20   //RS485 Direction control
+
+#define RS485Serial Serial1
+#endif
+
+#ifdef USE_ESP8266_TCP
+#include <ESP8266WiFi.h>
+#include <ModbusIP_ESP8266.h>
+ModbusIP mb;
+#endif
 
 // Action handler. Add all your actions mapped by action_id in rs485_node of Lua script
 void process_actions() {
-	if (holdingRegs[ACTIONS] == 0)
+	if (mb.Hreg(ACTIONS) == 0)
 		return;
 
-	switch (holdingRegs[ACTIONS]) {
+	switch (mb.Hreg(ACTIONS)) {
 		case 1 : // Put here code for Reset
 			Serial.println("[Reset] action fired");
-			digitalWrite(LED_BUILTIN, LOW);
+			digitalWrite(LED_BUILTIN, HIGH);
 			break;
 		case 2 : // Put here code for Complete
 			Serial.println("[Complete] action fired");
-			digitalWrite(LED_BUILTIN, HIGH);
+			digitalWrite(LED_BUILTIN, LOW);
 			break;
 		default:
 			break;
 	}
 
 	// Signal that action was processed
-	holdingRegs[ACTIONS] = 0;
+	mb.Hreg(ACTIONS, 0);
 }
 
 /////////////////////////////////////////////////////////////////
 
+void modbus_set(word event, word value) {
+	mb.Hreg(event, value);
+}
+
 void modbus_setup() {
-	Serial.println("Serial ModBus Slave BOXES:2 for lua/Aliens.lua");
+	Serial.println("ModBus Slave BOXES:2 for lua/Aliens.lua");
 
-	/* parameters(long baudrate,
-				  unsigned char ID,
-				  unsigned char transmit enable pin,
-				  unsigned int holding registers size)
-	*/
+#ifndef USE_ESP8266_TCP
+	mb.config(&RS485Serial, 57600, SSerialTxControl);
+	mb.setSlaveId(2);
+#else
+	mb.config("Aliens", "123123");
+  WiFi.config(IPAddress(2), IPAddress(), IPAddress(), IPAddress(), IPAddress());
 
-	modbus_configure(57600, 2, SSerialTxControl, TOTAL_REGS_SIZE);
-	holdingRegs[ACTIONS] = 0;
-	holdingRegs[MINOR_FAILURE] = 0;
-	holdingRegs[MAJOR_FAILURE] = 0;
-	holdingRegs[COMPLETE] = 0;
-	// Sample calls
-	pinMode(LED_BUILTIN, OUTPUT);
-	// buttonStatus_setup(MINOR_FAILURE, <buttonPin>);
+  Serial.print("Connecting to Aliens ");
+  while (WiFi.status() != WL_CONNECTED) {
+	delay(500);
+	Serial.print(".");
+  }
+
+  Serial.println(" CONNECTED!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Netmask: ");
+  Serial.println(WiFi.subnetMask());
+
+  Serial.print("Gateway: ");
+  Serial.println(WiFi.gatewayIP());
+#endif
+
+	mb.addHreg(ACTIONS, 0);
+	mb.addHreg(MINOR_FAILURE, 0);
+	mb.addHreg(MAJOR_FAILURE, 0);
+	mb.addHreg(COMPLETE, 0);
+
+	pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output (D4)
 }
 
 
 void modbus_loop() {
-	holdingRegs[TOTAL_ERRORS] = modbus_update(holdingRegs);
+	mb.task();              // not implemented yet: mb.Hreg(TOTAL_ERRORS, mb.task());
 	process_actions();
-
-	// Notify main console of local events
-	// holdingRegs[MINOR_FAILURE] = 1;
-	// holdingRegs[MAJOR_FAILURE] = 1;
-	// holdingRegs[COMPLETE] = 1;
-
-
-	// Sample calls
-	// buttonStatus(MINOR_FAILURE, <buttonPin>);
 }

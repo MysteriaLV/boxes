ed0a31a7fc29f69c699459be11ed5ec2a933b1b4
Pavel Veretennikov <vermut@kid.lv>
2017-07-03 23:45:17 +0300

    removed non-existing items working test mode

--- src/Boxes.cpp
+++ src/Boxes.cpp
@@ -20,7 +20,7 @@ void setup() {
 	button5.begin(46);
 	button6.begin(48);
 	button7.begin(50);
-	button8.begin(52);
+	// button8.begin(52);   Missing
 
 	led1.begin(23, true);
 	led2.begin(25, true);
@@ -29,7 +29,7 @@ void setup() {
 	led5.begin(31, true);
 	led6.begin(33, true);
 	led7.begin(35, true);
-	led8.begin(37, true);
+	led8.begin(37, true).on();  // Missing
 
 	ufo1.begin(39, true);
 	ufo2.begin(41, true);
@@ -38,9 +38,9 @@ void setup() {
 	ufo5.begin(47, true);
 	ufo6.begin(49, true);
 	ufo7.begin(51, true);
-	ufo8.begin(53, true);
+	ufo8.begin(53, true).on();  // Missing
 
-	door1.begin(22, true).trace(Serial);
+	door1.begin(22, true).on(); // Always open
 	door2.begin(24, true);
 	door3.begin(26, true);
 	door4.begin(28, true);
@@ -59,38 +59,35 @@ void setup() {
 	button5.onPress(door6, door6.EVT_OFF).onRelease(door6, door6.EVT_ON);
 	button6.onPress(door7, door7.EVT_OFF).onRelease(door7, door7.EVT_ON);
 	button7.onPress(door8, door8.EVT_OFF).onRelease(door8, door8.EVT_ON);
-	button8.onPress(door1, door1.EVT_OFF).onRelease(door1, door1.EVT_ON);
 
-	led1.blink(500).start();
-	led2.blink(500).start();
-	led3.blink(500).start();
-	led4.blink(500).start();
-	led5.blink(500).start();
-	led6.blink(500).start();
-	led7.blink(500).start();
-	led8.blink(500).start();
+	led1.blink(500, 500).start();
+	led2.blink(500, 500).start();
+	led3.blink(500, 500).start();
+	led4.blink(500, 500).start();
+	led5.blink(500, 500).start();
+	led6.blink(500, 500).start();
+	led7.blink(500, 500).start();
 
-	ufo1.blink(500).start();
-	ufo2.blink(500).start();
-	ufo3.blink(500).start();
-	ufo4.blink(500).start();
-	ufo5.blink(500).start();
-	ufo6.blink(500).start();
-	ufo7.blink(500).start();
-	ufo8.blink(500).start();
+	ufo1.blink(500, 500).start();
+	ufo2.blink(500, 500).start();
+	ufo3.blink(500, 500).start();
+	ufo4.blink(500, 500).start();
+	ufo5.blink(500, 500).start();
+	ufo6.blink(500, 500).start();
+	ufo7.blink(500, 500).start();
 
-	test_mode_timer1.begin(10)
-			.repeat(NUM_LEDS)
-			.onTimer( [] ( int idx, int v, int up ) {
-				multipartLedRibbon.fill_sold_ext(0, NUM_LEDS, CRGB::Gold);
-				multipartLedRibbon.fill_sold_ext(0, v, CRGB::White);
-				multipartLedRibbon.show();
-			})
-			.onFinish(test_mode_timer2, test_mode_timer1.EVT_START)
-			.start();
-
-	test_mode_timer2.begin(1)
-			.onFinish(test_mode_timer1, test_mode_timer1.EVT_START);
+//	test_mode_timer1.begin(10)
+//			.repeat(NUM_LEDS)
+//			.onTimer( [] ( int idx, int v, int up ) {
+//				multipartLedRibbon.fill_sold_ext(0, NUM_LEDS, CRGB::Gold);
+//				multipartLedRibbon.fill_sold_ext(0, v, CRGB::White);
+//				multipartLedRibbon.show();
+//			})
+//			.onFinish(test_mode_timer2, test_mode_timer1.EVT_START)
+//			.start();
+//
+//	test_mode_timer2.begin(1)
+//			.onFinish(test_mode_timer1, test_mode_timer1.EVT_START);
 #endif
 }
 
